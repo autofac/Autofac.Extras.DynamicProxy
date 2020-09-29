82026f27961be43d248ae6dda19c9ba9f58bacf4
alexmg <alex.meyergleaves@gmail.com>
2020-09-29 16:30:26 +1000

    Move properties from AssemblyInfo.cs into csproj

--- src/Autofac.Extras.DynamicProxy/Properties/AssemblyInfo.cs
+++ src/Autofac.Extras.DynamicProxy/Properties/AssemblyInfo.cs
@@ -1,9 +1,5 @@
 ﻿using System;
-using System.Reflection;
-using System.Resources;
 using System.Runtime.InteropServices;
 
-[assembly: AssemblyTitle("Autofac.Extras.DynamicProxy")]
 [assembly: CLSCompliant(false)]
 [assembly: ComVisible(false)]
-[assembly: NeutralResourcesLanguage("en-US")]
