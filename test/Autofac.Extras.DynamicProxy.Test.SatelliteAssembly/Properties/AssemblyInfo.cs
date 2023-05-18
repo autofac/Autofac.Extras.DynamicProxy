b0492fd3404df1ca4bd1d3ff7faa0e3b727c956f
Travis Illig <tillig@paraesthesia.com>
2023-05-18 07:06:26 -0700

    Added missing CLSCompliant tag on satellite asm.

---
+++ test/Autofac.Extras.DynamicProxy.Test.SatelliteAssembly/Properties/AssemblyInfo.cs
@@ -0,0 +1,4 @@
+﻿// Copyright (c) Autofac Project. All rights reserved.
+// Licensed under the MIT License. See LICENSE in the project root for license information.
+
+[assembly: CLSCompliant(false)]
